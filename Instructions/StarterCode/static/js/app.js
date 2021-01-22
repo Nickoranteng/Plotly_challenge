565d2e07e9c84470711f5ab6909c6d82deeb79e0
Nickoranteng <nickoranteng@gmail.com>
2021-01-21 23:08:57 -0500

    ploting bar and bubble chats

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,26 +1,72 @@
-function init() {
-  // Grab a reference to the dropdown select element
+showOptions();
+showData();
+optionChanged();
 
-  // Use the list of sample names to populate the select options
-  d3.json("/samples").then((data) => {
+function showOptions() {
+    d3.json('/samples').then(data => {
+
+        var { names } = data;
+        names.forEach(name => {
+            d3.select('select').append('option').text(name);
+        });
+    });
+};
+
+function showData() {
+    d3.json('/samples').then(data => {
+        var { metadata, samples } = data;
+        var selection = d3.select('select').property('value');
+
+        d3.select('.panel-body').html('');
+        metadata = metadata.filter(obj => obj.id == selection)[0];
+        Object.entries(metadata).forEach(([key, val]) => {
+            d3.select('.panel-body').append('h5').text(`${key.toUpperCase()}: ${val}`);
+        });
+
+        samples = samples.filter(obj => obj.id == selection)[0];
+        var { otu_ids, otu_labels, sample_values } = samples;
+
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+                size: sample_values
+            }
+        }];
+
+        Plotly.newPlot('bubble', bubbleData);
+
+        // BAR
+
+        d3.json("/samples").then((sdata) => {
+            subject_id = selection;
+            var samples = sdata['samples'];
+            var filtered_samples = samples.filter(s => s['id'] == subject_id);
+            var sample_values = filtered_samples.map(s => s['sample_values']);
+            var otu_ids = filtered_samples.map(s => s['otu_ids']);
+            var otu_labels = ''
+            var x = sample_values[0].slice(0,10).reverse();
+            var y = otu_ids[0].slice(0,10).map(o => `OTU ${o}`).reverse();
+            var data = [{
+              type: 'bar',
+              x: x,
+              y: y,
+              orientation: 'h'
+            }];
+            
+            Plotly.newPlot('bar', data);
+        });
+
+    });
     
-    // your-code-here
 
-    // Use the first sample from the list to build the initial plots
-  });
 }
 
-/*
-   Hints: Create additional functions to build the charts,
-          build the gauge chart, set up the metadata,
-          and handle the event listeners
-
-   Recommended function names:
-    optionChanged() 
-    buildChart()
-    buildGauge()
-    buildMetadata()
-*/
-
-// Initialize the dashboard
-init();
+function optionChanged() {
+    d3.select('select').on('change', function () {
+        showData();
+    })
+}
